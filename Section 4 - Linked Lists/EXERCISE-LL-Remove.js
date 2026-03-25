20d38ea5e29603c004b8d0ff4e674813ca282b0c
NitinRajput98 <nitin98811234@gmail.com>
2026-03-26 01:27:02 +0530

    LL : Remove

--- Section 4 - Linked Lists/EXERCISE-LL-Remove.js
+++ Section 4 - Linked Lists/EXERCISE-LL-Remove.js
@@ -134,12 +134,17 @@ class LinkedList {
         return true;
     }
  
-	/// WRITE REMOVE METHOD HERE ///
-	//                            //
-	//                            //
-	//                            //
-	//                            //
-	////////////////////////////////
+	remove(index){
+        if(index<0||index>=this.length) return undefined
+        if(index === 0) return this.shift()
+        if(index === this.length-1) return this.pop()
+        const before = this.get(index-1)
+        const temp = before.next()
+        before.next = temp.next
+        temp.next = null
+        this.length--
+        return temp
+    }
 
 }
 
